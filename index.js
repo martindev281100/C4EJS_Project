ac067dbbdfe4163e3c91c0849b0ced7463cf874c
mitximinaj <thangflymit@gmail.com>
2020-07-23 21:07:55 +0700

    fs

--- index.js
+++ index.js
@@ -1,14 +1,280 @@
+let data = [
+    {
+        question: "What causes ocean tides?",
+        correctAnswer: "The pull of gravity from the Moon and the Sun",
+        wrongAnswers: [
+            "The pull of gravity from the Sun",
+            "The pull of gravity from the Moon",
+            "The Earth spin around its axis",
+        ],
+        hint: "Not only one cause",
+    },
+    {
+        question: "What is the biggest animal that ever lived?",
+        correctAnswer: "Blue whale",
+        wrongAnswers: [
+            "King Kong",
+            "Tyrannosaurus rex",
+            "Sperm whale",
+        ],
+        hint: "Fish",
+    },
+    {
+        question: "What is culture?",
+        correctAnswer: "The features of everyday life shared by a group",
+        wrongAnswers: [
+            "A person's individual traits",
+            "Something different that one person does in their daily life",
+            "Having respect and acceptance for people different than you",
+        ],
+        hint: "Stem from more than one person",
+    },
+    {
+        question: "Who sets the standards that create a culture?",
+        correctAnswer: "All People",
+        wrongAnswers: [
+            "Family Elders",
+            "Artists",
+            "Governments",
+        ],
+        hint: "Something that is normal",
+    },
+    {
+        question: "Which is not a danger to explorers searching for the lost city?",
+        correctAnswer: "Rabid monkeys",
+        wrongAnswers: [
+            "Deadly snakes",
+            "Hungry jaguars",
+            "Poisonous insects",
+        ],
+        hint: "Animal that contains non-contagious disease",
+    },
+    {
+        question: "What is a property that leads to electromagnetic interactions between particles of matter?",
+        correctAnswer: "Electric charge",
+        wrongAnswers: [
+            "Static electricity",
+            "Electric conduction",
+            "Semi-conduction",
+        ],
+        hint: "Intake of electricity",
+    },
+    {
+        question: "Which of the following is not an element of which color is composed?",
+        correctAnswer: "Detail (definition)",
+        wrongAnswers: [
+            "Value (hue)",
+            "Intensity (saturation)",
+            "Tonality (luminance)",
+        ],
+        hint: "HSL",
+    },
+    {
+        question: "What is the measurement of Earth's gravity?",
+        correctAnswer: "9.8 m/s2",
+        wrongAnswers: [
+            "8.2 m/s2",
+            "6.9 m/s2",
+            "3.5 m/s2",
+        ],
+        hint: "Can be rounded up to 10 m/s2",
+    },
+    {
+        question: "BarmBrack, a fruitcake, is a Halloween Irish tradition. This cake contains:",
+        correctAnswer: "Objects baked into the bread used as a fortune-telling game",
+        wrongAnswers: [
+            "Special powers that will make you see ghosts",
+            "Chili and different spices",
+            "Meat",
+        ],
+        hint: "Something for prophecy",
+    },
+    {
+        question: "Which of the following is NOT a major component of Cell Theory?",
+        correctAnswer: "All organisms contain eukaryotic cells",
+        wrongAnswers: [
+            "All organisms are made of 1 or more cells",
+            "The cell is the basic unit of life",
+            "All cells come from pre-existing cells",
+        ],
+        hint: "Not the fundamental root of an organism",
+    }
+];
+
+let math = [
+    {
+        question: "1 + 1 = ?",
+        correctAnswer: "2",
+        wrongAnswers: [
+            "3",
+            "4",
+            "5",
+        ],
+    },
+    {
+        question: "1 x 10 = ?",
+        correctAnswer: "10",
+        wrongAnswers: [
+            "30",
+            "40",
+            "50",
+        ],
+    },
+    {
+        question: "11 x 12 = ?",
+        correctAnswer: "132",
+        wrongAnswers: [
+            "69",
+            "145",
+            "213",
+        ],
+    },
+    {
+        question: "99 - 72 = ?",
+        correctAnswer: "27",
+        wrongAnswers: [
+            "34",
+            "49",
+            "52",
+        ],
+    },
+    {
+        question: "3 x 2 = ?",
+        correctAnswer: "6",
+        wrongAnswers: [
+            "9",
+            "10",
+            "5",
+        ],
+    },
+    {
+        question: "0 / 1 = ?",
+        correctAnswer: "0",
+        wrongAnswers: [
+            "1",
+            "100",
+            "10",
+        ],
+    },
+    {
+        question: "99 / 3 = ?",
+        correctAnswer: "33",
+        wrongAnswers: [
+            "66",
+            "49",
+            "11",
+        ],
+    },
+    {
+        question: "3! = ?",
+        correctAnswer: "6",
+        wrongAnswers: [
+            "7",
+            "9",
+            "10",
+        ],
+    },
+    {
+        question: "9 ^ 2 = ?",
+        correctAnswer: "81",
+        wrongAnswers: [
+            "67",
+            "63",
+            "72",
+        ],
+    },
+    {
+        question: "sin(0) = ?",
+        correctAnswer: "0",
+        wrongAnswers: [
+            "1",
+            "0.3",
+            "0.6",
+        ],
+    },
+];
+
+for(let i = 0; i < data.length; i ++) {
+    let arr = [data[i].question, data[i].correctAnswer, data[i].wrongAnswers, data[i].hint];
+    showItems(arr);
+}
+
+function addItem() {
+    let newData = {
+        question: document.getElementById("input_question").value,
+        correctAnswer: document.getElementById("input_correct").value,
+        wrongAnswers: document.getElementById("input_wrong").value,
+        hint: document.getElementById("input_hint").value,
+    }
+    newData.wrongAnswers = newData.wrongAnswers.split(", ");
+    data.push(newData);
+    let arr = [newData.question, newData.correctAnswer, newData.wrongAnswers, newData.hint];
+    showItems(arr);
+}
+
+function showItems(arr) {
+    let table = document.getElementById("table_questions");
+    let tableRow = document.createElement("tr");
+    for(let i = 0; i < 4; i ++) {
+        let tableDetail = document.createElement("td");
+        if(i == 2) tableDetail.innerHTML = arr[i].join(", ");
+        else tableDetail.innerHTML = arr[i];
+        switch(i) {
+            case 0: tableDetail.classList.add("question_content"); break;
+            case 1: tableDetail.classList.add("correct_answer"); break;
+            case 2: tableDetail.classList.add("wrong_answer"); break;
+            case 3: tableDetail.classList.add("col_hint"); break;
+        }
+        tableRow.appendChild(tableDetail);
+    }
+  
+    let tableDetail = document.createElement("td");
+    tableDetail.classList.add("col_action");
+    
+    let buttonUpdate = document.createElement("button");
+    buttonUpdate.innerHTML = "Update";
+    buttonUpdate.onclick = function () {
+        let i = buttonUpdate.parentNode.parentNode.rowIndex;
+        table.rows[i].contentEditable = "true";
+        table.rows[i].onfocusout = function () {
+            table.rows[i].contentEditable = "false";
+            data[i - 2].question = table.rows[i].cells[0].innerHTML;
+            data[i - 2].correctAnswer = table.rows[i].cells[1].innerHTML;
+            data[i - 2].wrongAnswers = table.rows[i].cells[2].innerHTML;
+            data[i - 2].hint = table.rows[i].cells[3].innerHTML;
+        }
+    }
+    buttonUpdate.classList.add("btn-warning");
+    buttonUpdate.classList.add("btn");
+    buttonUpdate.classList.add("btn_update");
+  
+    let buttonDelete = document.createElement("button");
+    buttonDelete.innerHTML = "Delete";
+    buttonDelete.onclick = function () {
+        let i = buttonDelete.parentNode.parentNode.rowIndex;
+        document.getElementById("table_questions").deleteRow(i);
+        data.splice(i - 2, 1);
+    };
+    buttonDelete.classList.add("btn-danger");
+    buttonDelete.classList.add("btn");
+    buttonDelete.classList.add("btn_delete");
+  
+    tableDetail.appendChild(buttonUpdate);
+    tableDetail.appendChild(buttonDelete);
+    tableRow.appendChild(tableDetail);
+    table.appendChild(tableRow);
+}
+
 let unauthorized_form = function () {
     document.getElementById('table_questions').hidden = true;
-    document.getElementById("col_action").hidden = true;
+    document.getElementsByClassName("col_action").hidden = true;
     document.getElementById("col_action_title").hidden = true;
-    document.getElementById("btn_add").style.display = 'none';
     document.getElementById('btn_logIn').hidden = false;
     document.getElementById('btn_register').hidden = false;
     document.getElementById('btn_logOut').hidden = true;
     document.getElementById('input_account').style.display = 'block';
     document.getElementById('input_password').style.display = 'block';
-    document.getElementById('col_hint').hidden = true;
+    document.getElementsByClassName('col_hint').hidden = true;
     document.getElementById('col_hint_title').hidden = true;
     document.getElementById('btn_add').hidden = true;
     document.getElementById('form-add').hidden = true;
@@ -16,15 +282,15 @@ let unauthorized_form = function () {
 unauthorized_form();
 
 let user_authorized = function () {
-    document.getElementById("col_action").hidden = true;
+    document.getElementsByClassName("col_action").hidden = true;
     document.getElementById("col_action_title").hidden = true;
-    document.getElementById("btn_add").style.display = 'none';
+    document.getElementById("btn_add").hidden = false;
     document.getElementById('btn_logIn').hidden = true;
     document.getElementById('btn_logOut').hidden = false;
     document.getElementById('btn_register').hidden = true;
     document.getElementById('input_account').style.display = 'block';
     document.getElementById('input_password').style.display = 'block';
-    document.getElementById('col_hint').hidden = true;
+    document.getElementsByClassName('col_hint').hidden = true;
     document.getElementById('col_hint_title').hidden = true;
     document.getElementById('table_questions').hidden = false;
     document.getElementById('form_register').hidden = true;
@@ -32,27 +298,26 @@ let user_authorized = function () {
 
 let authorized_form = function () {
     document.getElementById('table_questions').hidden = false;
-    document.getElementById("col_action").hidden = false;
+    document.getElementsByClassName("col_action").hidden = false;
     document.getElementById("col_action_title").hidden = false;
-    document.getElementById("btn_add").style.display = 'block';
+    document.getElementById("btn_add").hidden = false;
     document.getElementById('btn_logOut').hidden = false;
     document.getElementById('btn_logIn').style.display = 'none';
     document.getElementById('btn_register').hidden = true;
     document.getElementById('input_account').style.display = 'none';
     document.getElementById('input_password').style.display = 'none';
-    document.getElementById('col_hint').hidden = false;
+    document.getElementsByClassName('col_hint').hidden = false;
     document.getElementById('col_hint_title').hidden = false;
 
 }
 
-
 let unclicked_btn_add = function () {
-    document.getElementById('form-add').style.display = 'none';
+    document.getElementById('form-add').hidden = true;
 }
 unclicked_btn_add();
 
 let clicked_btn_add = function () {
-    document.getElementById('form-add').style.display = 'block';
+    document.getElementById('form-add').hidden = false;
 }
 
 const btn_add = document.getElementById('btn_add');
@@ -93,12 +358,10 @@ check = function () {
     }
 };
 
-
 const btn_logOut = document.getElementById('btn_logOut')
 btn_logOut.addEventListener('click', function () {
     unauthorized_form();
 })
-
 
 let validate_registration = function () {
     var input_fName = document.getElementById('input_fName').value;
