7a315eb95e67da62bdd719d6c6dabd327b763041
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-07-26 09:23:41 +0700

    update index.js + index html

--- index.js
+++ index.js
@@ -284,13 +284,14 @@ let unauthorized_form = function () {
     document.getElementById('col_hint_title').hidden = true;
     document.getElementById('btn_add').hidden = true;
     document.getElementById('form-add').hidden = true;
+    document.getElementById('form_quizz').hidden = true;
 }
 unauthorized_form();
 
 let user_authorized = function () {
     document.getElementsByClassName("col_action").hidden = true;
     document.getElementById("col_action_title").hidden = true;
-    document.getElementById("btn_add").hidden = false;
+    document.getElementById("btn_add").hidden = true;
     document.getElementById('btn_logIn').hidden = true;
     document.getElementById('btn_logOut').hidden = false;
     document.getElementById('btn_register').hidden = true;
@@ -298,8 +299,9 @@ let user_authorized = function () {
     document.getElementById('input_password').style.display = 'block';
     document.getElementsByClassName('col_hint').hidden = true;
     document.getElementById('col_hint_title').hidden = true;
-    document.getElementById('table_questions').hidden = false;
+    document.getElementById('table_questions').hidden = true;
     document.getElementById('form_register').hidden = true;
+    document.getElementById('form_quizz').hidden = false;
 }
 
 let authorized_form = function () {
@@ -341,13 +343,15 @@ btn_add_item.addEventListener('click', function () {
 const btn_logIn = document.getElementById('btn_logIn');
 btn_logIn.addEventListener('click', function () {
     check();
+    document.getElementById("input_account").value = '';
+    document.getElementById("input_password").value = '';
 })
 
 let arr_account = [];
 
 check = function () {
-    const account = document.getElementById("input_account").value;
-    const password = document.getElementById("input_password").value;
+    let account = document.getElementById("input_account").value;
+    let password = document.getElementById("input_password").value;
     console.log(account + password);
     if (account == 'admin' && password == 1234) {
         authorized_form();
@@ -361,6 +365,7 @@ check = function () {
             unauthorized_form();
         }
     }
+    
 };
 
 const btn_logOut = document.getElementById('btn_logOut')
@@ -385,6 +390,10 @@ let validate_registration = function () {
         alert('Password must be match!');
     } else {
         arr_account.push(newObj);
+       let alert_success = document.getElementById('alert_success');
+       setTimeout(function(){
+        
+       })
     }
 }
 let unclicked_btn_reg = function () {
@@ -405,4 +414,213 @@ btn_reg.addEventListener('click', function () {
     console.log('clicked')
     validate_registration();
     console.log(arr_account);
+    unclicked_btn_reg();
 })
+
+
+chonchude = function () {
+    document.getElementById("chude").style.display = 'block';
+
+}
+playGame = function () {
+    document.getElementById("chude").style.display = 'block';
+    document.getElementById("play").style.display = 'none';
+    document.getElementById("formGame").style.display = 'none';
+}
+let i = 0;
+var out = false;
+let userscore = 0;
+let login = false;
+let iAcc;
+checkAcc = function () {
+    let idAcc = document.getElementById("input_account").value;
+    let passAcc = document.getElementById("input_password").value;
+
+    for (let i in arr_account) {
+        if (arr_account[i].email == idAcc) {
+
+            login = true;
+            iAcc = i;
+            alert(iAcc);
+        }
+    }
+}
+start = function (chudeinput) {
+    
+    document.getElementById("chude").style.display = 'none';
+    document.getElementById("formGame").style.display = 'block';
+
+    let score = 0;
+
+    var chuDe = chudeinput;
+    let c = [];
+    run = function () {
+
+        c.push(chuDe[i].wrongAnswers[0]);
+        c.push(chuDe[i].wrongAnswers[1]);
+        c.push(chuDe[i].wrongAnswers[2]);
+        c.push(chuDe[i].correctAnswer);
+
+        document.getElementById("cauHoi").innerHTML = chuDe[i].question;
+        document.getElementById("score").innerHTML = "Diem hien tai cua ban : " + score;
+        for (let i1 = 1; i1 < 5; i1++) {
+            rand = Math.floor(Math.random() * c.length);
+            document.getElementById('c' + i1).value = c[rand];
+            document.getElementById("c" + i1).style.background = 'rgb(64, 204, 8)';
+            c.splice(rand, 1);
+        };
+    }
+
+    run();
+    logout = function () {
+        i = 0;
+        score = 0;
+        userscore = 0;
+        login = false;
+        stop();
+        chonchude();
+        alert("logout");
+        document.getElementById("account").innerHTML = "";
+    };
+    if (login == true) {
+        alert('ok');
+    };
+    if (login == true) {
+        userscore = arr_account[iAcc].score;
+    };
+    check = function (valuec, idtab) {
+        let kq = valuec;
+        let idthe = 'c' + idtab;
+        let idtrue = 'c';
+
+        for (let j in chuDe) {
+            if (kq == chuDe[j].correctAnswer) {
+                score++;
+
+                xanh = setTimeout(() => {
+                    document.getElementById("thongBaoKq").innerHTML = 'dung';
+                    if (document.getElementById(idthe).value == chuDe[j].correctAnswer) {
+                        document.getElementById(idthe).style.background = "green";
+
+                    }
+                }, 500);
+
+            } else {
+                vang = setTimeout(() => {
+                    document.getElementById("thongBaoKq").innerHTML = 'sai';
+                    for (let i4 = 1; i4 < 5; i4++) {
+                        if (document.getElementById(idtrue + i4).value == chuDe[j].correctAnswer) {
+                            document.getElementById(idtrue + i4).style.background = 'yellow';
+                        }
+                    }
+                }, 499);
+
+            }
+        }
+        let endGame = setTimeout(() => {
+            if (i == chuDe.length) {
+                console.log("end");
+                document.getElementById("formGame").style.display = 'none';
+                document.getElementById("play").style.display = 'block';
+                stop();
+                clearTimeout(reRun);
+                if (login == true) {
+                    console.log(arr_account);
+                }
+
+            }
+        }, 800);
+
+        // toMau(idthe);
+        i++;
+        reRun = setTimeout(run, 1000);
+
+    }
+
+    stop = function () {
+        if (login == true) {
+            arr_account[iAcc].score += score;
+        };
+
+        userscore += score;
+        document.getElementById("score").innerHTML = "Diem cua ban la : " + userscore;
+        i = 0;
+
+    }
+    // toMau = function (kq) {
+    //     document.getElementById(kq).style.background = "red";
+    //     a = setTimeout(() => {
+    //         document.getElementById(kq).style.background = "white";
+    //     }, 1000);
+    // }
+
+    doihint = () => {
+        document.getElementById("score").innerHTML = "Diem cua ban la : " + userscore;
+        console.log(score + ' ' + userscore);
+        if (chuDe[i].hint) {
+            if (score > 0) {
+                alert(chuDe[i].hint);
+                score -= 0.5;
+
+            } else if (score < 0.5 && userscore < 0.5) {
+                alert("ban khong du diem ");
+            }
+            else if (userscore > 0) {
+                alert(chuDe[i].hint);
+                score -= 0.5;
+            }
+        } else {
+            alert("not hint");
+        }
+
+
+    };
+    trogiup = function () {
+        score -= 0.5;
+        console.log(score);
+        let cc = 'c';
+
+
+        let arr = [];
+        for (let i3 = 1; i3 < 5; i3++) {
+            //let rand = Math.ceil(Math.random()*4);
+            if (document.getElementById(cc + i3).value == chuDe[i].correctAnswer) {
+                arr.push(i3);
+                for (let i4 = 1; i4 < 5; i4++) {
+                    let rand = Math.ceil(Math.random() * 4);
+                    if (rand != i3) {
+                        arr.push(rand);
+                        break;
+                    }
+                }
+
+            }
+        } console.log(arr);
+        for (let i5 = 1; i5 < 5; i5++) {
+            if (i5 != arr[0] && i5 != arr[1]) {
+                document.getElementById(cc + i5).value = "x";
+            }
+        }
+    }
+
+
+    quay = function () {
+        document.getElementById("inscore").style.display = 'block';
+        document.getElementById("quay").style.display = 'block';
+        let diemcuoc = document.getElementById("inscore").value;
+        console.log(diemcuoc);
+    };
+    quayso = function () {
+        let diemdoi = document.getElementById("inscore").value;
+        let ran = Math.floor(Math.random() * ((diemdoi * 2) + 1));
+        if (userscore >= diemdoi) {
+            userscore -= diemdoi;
+            console.log("ban nhan duoc " + ran + " diem");
+            userscore += ran;
+            console.log(userscore + " D");
+        }
+        else {
+            console.log("ban khong du diem");
+        }
+    }
+};
